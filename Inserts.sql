1a5c411dd3daaee7c947f093b73d7083d97b631e
Alexandre Galvão Vida <alexandre.vida@valecard.com.br>
2019-07-19 00:07:57 -0300

    Add novos aulas feitas

---
+++ Inserts.sql
@@ -0,0 +1,5 @@
+insert into Movimentacao(data, valor, conta_id, descricao, tipo) values ('2019-07-18 18:01:07', 80.0, 2, "Restaurante", "SAIDA");
+insert into Movimentacao(data, valor, conta_id, descricao, tipo) values ('2019-07-18 19:31:12', 100.0, 2, "Cinema", "SAIDA");
+insert into Movimentacao(data, valor, conta_id, descricao, tipo) values ('2019-07-19 10:01:54', 40.0, 2, "Café da manhã", "SAIDA");
+insert into Movimentacao(data, valor, conta_id, descricao, tipo) values ('2019-07-19 15:20:13', 20.0, 2, "Lanche", "SAIDA");
+insert into Movimentacao(data, valor, conta_id, descricao, tipo) values ('2017-01-26 18:01:07', 80.0, 2, "Restaurante", "SAIDA");
